620fc7b94504a40a8ce08e9e2eba0d6c4056bc94
oneofmanyadams <adam.plazewski@gmail.com>
2024-04-30 20:29:22 -0600

    Added comments for private functions.

--- optionprompt.go
+++ optionprompt.go
@@ -41,6 +41,9 @@ func New(question string) OptionPrompt {
 // Private Functions
 ////////////////////////////////////////////////////////////////////////////////
 
+// optionExists determines if the provided option_name already exists in s.Options.
+// If it does the first returned calue is the key and the 2nd return value is true.
+// If it doesn't the next available key is returned and the 2nd return value is false.
 func (s *OptionPrompt) optionExists(option_name string) (int, bool) {
 	for k, v := range s.Options {
 		if option_name == v {
@@ -50,6 +53,7 @@ func (s *OptionPrompt) optionExists(option_name string) (int, bool) {
 	return len(s.Options), false
 }
 
+// addOption adds an option to s.Options and returns it's key.
 func (s *OptionPrompt) addOption(option_name string) int {
 	s.Options = append(s.Options, option_name)
 	return len(s.Options) - 1
